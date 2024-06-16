c48702ec98b6a48ddd68054189cf30db70398029
mirza-sync <mirzasyahmi123@gmail.com>
2024-06-16 15:17:44 +0800

    Generate stars at random postion

--- main.js
+++ main.js
@@ -4,9 +4,9 @@ import { OrbitControls } from 'three/examples/jsm/Addons.js';
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 500)
 camera.position.z = 100
-camera.lookAt( 0, 0, 0 );
+camera.lookAt(0, 0, 0);
 
-const renderer = new THREE.WebGLRenderer({antialias: true});
+const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement)
 
@@ -28,6 +28,21 @@ const gridHelper = new THREE.GridHelper(200, 50)
 scene.add(gridHelper)
 
 const controls = new OrbitControls(camera, renderer.domElement);
+
+function generateStar() {
+  const geometry = new THREE.SphereGeometry(0.25, 24, 24)
+  const material = new THREE.MeshStandardMaterial({ color: 0xffffff })
+  const star = new THREE.Mesh(geometry, material)
+
+  const [x, y, z] = Array(3)
+    .fill()
+    .map(() => THREE.MathUtils.randFloatSpread(100))
+
+  star.position.set(x, y, z)
+  scene.add(star)
+}
+
+Array(200).fill().forEach(() => generateStar())
 
 function animate() {
   cube.rotation.x += 0.01
