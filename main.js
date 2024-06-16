4740d8c44f1c118b6ba1b079d77c0af387ac054f
mirza-sync <mirzasyahmi123@gmail.com>
2024-06-16 12:26:29 +0800

    Draw some lines

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 import * as THREE from 'three'
 
 const scene = new THREE.Scene()
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 500)
+camera.position.z = 100
+camera.lookAt( 0, 0, 0 );
 
 const renderer = new THREE.WebGLRenderer({antialias: true});
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -12,7 +14,14 @@ const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
 const cube = new THREE.Mesh(geometry, material)
 scene.add(cube)
 
-camera.position.z = 5
+const points = []
+points.push(new THREE.Vector3(-10, 0, 0))
+points.push(new THREE.Vector3(0, 10, 0))
+points.push(new THREE.Vector3(10, 0, 0))
+const lineGeometry = new THREE.BufferGeometry().setFromPoints(points)
+const lineMaterial = new THREE.LineBasicMaterial({ color: 0x0000ff })
+const line = new THREE.Line(lineGeometry, lineMaterial)
+scene.add(line)
 
 function animate() {
   cube.rotation.x += 0.01
